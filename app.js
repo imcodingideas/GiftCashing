617d5ad5b4020e124ed3de391f6d8b845314d38b
Joseph Chambers <joseph@michael-chambers.com>
2016-12-04 03:35:58 -0700

    Removed duplicate local variable

--- app.js
+++ app.js
@@ -66,7 +66,6 @@ app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.flash('error');
     res.locals.success = req.flash('success');
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
