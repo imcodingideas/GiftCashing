33297db75b2d315df46f7bc2874fe848cd673b42
Joseph Chambers <joseph@michael-chambers.com>
2016-12-05 20:44:56 -0700

    Created Gifts
    BUG. Cannot PUT /users/584123ab0633bc1759fd1dcc/gifts/?_method=PUT

--- routes/gifts.js
+++ routes/gifts.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     router = express.Router({mergeParams: true}),
     User = require('../models/user'),
-    Gifts = require('../models/gift'),
+    Gift = require('../models/gift'),
     faker = require('faker');
 
 
@@ -16,6 +16,35 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// Create a Gift
+router.post('/', function(req, res) {
+    // get data from form and add to gift array.
+    let username = req.body.username,
+        giftNumber = req.body.giftNumber,
+        date = req.body.date,
+        giftDescription = req.body.giftDescription,
+        giftAmount = req.body.giftAmount,
+        giftCode = req.body.giftCode,
+        redeemCode = req.body.redeemCode,
+        passCode = req.body.passCode,
+        senderFirstName = req.body.senderFirstName,
+        senderLastName = req.body.senderLastName,
+        giftMessage = req.body.giftMessage,
+        newGift = { username: username, giftNumber: giftNumber, date: date, giftDescription: giftDescription, giftAmount: giftAmount, giftCode: giftCode, redeemCode: giftCode, redeemCode: redeemCode, passCode: passCode, senderFirstName: senderFirstName, senderLastName: senderLastName, giftMessage: giftMessage };
+
+     // create a new gift, and save to the database
+     Gift.create(newGift, function (err, newlyCreated) {
+        if(err) {
+            req.flash('error', err.message);
+        } else {
+            console.log(newlyCreated);
+            res.redirect('/')
+        }
+     });
+
+
+});
+
 router.get('/new', function (req, res, next) {
     User.findById(req.params.id, function (err, user) {
         if(err) {
@@ -25,5 +54,6 @@ router.get('/new', function (req, res, next) {
         }
     });
 });
+
 
 module.exports = router;
