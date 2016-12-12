2c78689058ea6ad7c9b155a1dcf120552cffa9ea
Joseph Chambers <joseph@michael-chambers.com>
2016-12-11 22:37:03 -0700

    Datepicker works
    - Added jQuery UI
    - Uninstalled datepicker-moment
    - Made datepicker work on to and from

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,10 +1,14 @@
 /**
  * Created by joseph on 12/11/16.
  */
+$( document ).ready(function() {
+    // close alert row
+    window.setTimeout(function() {
+        $('.alert').fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove();
+        });
+    }, 4000);
 
-// close alert row
-window.setTimeout(function() {
-    $('.alert').fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove();
-    });
-}, 4000);
+    $( '#datefrom' ).datepicker();
+    $( '#dateto' ).datepicker();
+} );
