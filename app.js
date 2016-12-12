4098d7abd833971908c01a81c0dd8ef2110515f7
Joseph Chambers <joseph@michael-chambers.com>
2016-12-11 20:57:03 -0700

    Added es6 promises as mongoose default promise

--- app.js
+++ app.js
@@ -63,25 +63,11 @@ app.use(function (req, res, next) {
     next();
 });
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
 app.use('/', indexRoute);
 app.use('/users', usersRoute);
 app.use('/gifts', giftsRoute);
 app.use('/search', searchRoute);
+
+mongoose.Promise = global.Promise;
 
 module.exports = app;
