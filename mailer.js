a2cf14c6c26d2ec7aa9f158b0b1bc9229fe5189e
Joseph Chambers <joseph@michael-chambers.com>
2017-01-01 20:14:41 -0700

    fixed bug

--- mailer.js
+++ mailer.js
@@ -1,15 +1,18 @@
 const nodemailer = require('nodemailer'),
       mg = require('nodemailer-mailgun-transport');
 
-let smtpTransport = nodemailer.createTransport(mg(auth));
-
-smtpTransport.sendMail({
-    host: 'smtp.mailgun.org',
+let auth = {
     auth: {
-        from: 'joseph@michael-chambers.com',
         api_key: 'key-8a976a9cba0ee1e86ec07d7398b544e8',
         domain: 'sandboxb1fd7de656fe403591f3528498bfd132.mailgun.org'
     }
+}
+
+let smtpTransport = nodemailer.createTransport(mg(auth));
+
+smtpTransport.sendMail({
+    from: 'joseph@michael-chambers.com',
+    host: 'smtp.mailgun.org',
 });
 
 module.exports = smtpTransport;
