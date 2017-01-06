488e978cfa760d6ae92b911cfca941cb76cd1728
Joseph Chambers <joseph@michael-chambers.com>
2017-01-05 19:25:18 -0700

    Codestyle

--- routes/users.js
+++ routes/users.js
@@ -16,77 +16,96 @@ router.get('/admin/users', middleware.isLoggedIn, (req, res, next) => {
 
   if (req.query.search) {
     let fuzzy = new RegExp(req.query.search, 'gi');
-    User.find({username: fuzzy}, (err, allUsers) => {
-      if (err) {
-        req.flash('error', err.message);
-      }
-      if (allUsers.length < 1) {
-        noMatch = 'No users were found based on that query ' + req.query.search;
-      }
+    User
+      .find({username: fuzzy},
+        (err, allUsers) => {
+          if (err) {
+            req.flash('error', err.message);
+          }
+          if (allUsers.length < 1) {
+            noMatch = 'No users were found based on that query ' + req.query.search;
+          }
 
-      res.render('admin/users/index', {
-        users: allUsers,
-        title: 'Search Results',
-        breadcrumbsName: 'Users',
-        noMatch: noMatch
-      });
-    });
+          res.render(
+            'admin/users/index', {
+              users: allUsers,
+              title: 'Search Results',
+              breadcrumbsName: 'Users',
+              noMatch: noMatch
+            });
+        });
   }
   // get all users from db
-  User.find({}, (err, allUsers) => {
-    if (err) {
-      req.flash('error', err.message);
-    }
+  User
+    .find({},
+      (err, allUsers) => {
+        if (err) {
+          req.flash('error', err.message);
+        }
 
-    res.render('admin/users/index', {
-      users: allUsers,
-      title: 'All Users',
-      breadcrumbsName: 'Users',
-      noMatch: noMatch
-    });
-  });
+        res.render(
+          'admin/users/index', {
+            users: allUsers,
+            title: 'All Users',
+            breadcrumbsName: 'Users',
+            noMatch: noMatch
+          });
+      });
 });
 
 // update user
-router.put('/dashboard/profile/', middleware.isLoggedIn, (req, res, next) => {
-  //find and update correct user
-  User.findByIdAndUpdate(req.params.id, req.body.user, (err, updatedUser) => {
-    if (err) {
-      return req.flash('error', err.message);
-    }
+router.put(
+  '/dashboard/profile/',
+  middleware.isLoggedIn,
+  (req, res, next) => {
+    //find and update correct user
+    User
+      .findByIdAndUpdate(req.params.id, req.body.user, (err, updatedUser) => {
+        if (err) {
+          return req.flash('error', err.message);
+        }
 
-    res.redirect('/admin/users/' + req.params.id + '/edit', {
-      user: updatedUser
-    });
+        res.redirect('/admin/users/' + req.params.id + '/edit', {
+          user: updatedUser
+        });
+      });
   });
-});
 
 // Edit User
-router.get('/dashboard/profile/:id/edit', middleware.isLoggedIn, (req, res, next) => {
-  User
-    .findOne({_id: req.params.id}, (err, foundUser) => {
-      res.render('dashboard/profile/edit', {
-        user: foundUser,
-        title: 'Member Profile',
-        breadcrumbsName: 'Member Profile'
+router.get(
+  '/dashboard/profile/:id/edit',
+  middleware.isLoggedIn,
+  (req, res, next) => {
+    User
+      .findOne({_id: req.params.id}, (err, foundUser) => {
+        res.render('dashboard/profile/edit', {
+          user: foundUser,
+          title: 'Member Profile',
+          breadcrumbsName: 'Member Profile'
+        });
       });
-    });
-});
-
-// Update User
-router.put('/dashboard/profile/:id', middleware.isLoggedIn, (req, res, next) => {
-  let userData = req.body.user;
-
-  //find and update user
-  User.findByIdAndUpdate(req.params.id, userData, (err, updatedUser) => {
-    if (err) {
-      return req.flash('error', err.message);
-    }
-
-    //redirect show page
-    res.redirect('/dashboard/profile/' + req.params.id + '/edit');
   });
 
-});
+// Update User
+router.put(
+  '/dashboard/profile/:id',
+  middleware.isLoggedIn,
+  (req, res, next) => {
+    let userData = req.body.user;
+
+    //find and update user
+    User
+      .findByIdAndUpdate(
+        req.params.id, userData,
+        (err, updatedUser) => {
+          if (err) {
+            return req.flash('error', err.message);
+          }
+
+          //redirect show page
+          res.redirect('/dashboard/profile/' + req.params.id + '/edit');
+        });
+
+  });
 
 module.exports = router;
