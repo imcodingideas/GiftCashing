e848f82a169778f4392c2904aaca09155c9bf7f5
Joseph Chambers <joseph@michael-chambers.com>
2017-01-05 18:08:20 -0700

    Share Page
    Added alias name to sidebar

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -25,4 +25,20 @@ router.get('/dashboard/received', middleware.isLoggedIn, (req, res) => {
   });
 });
 
+
+router.get('/dashboard/share', middleware.isLoggedIn, (req, res) => {
+  User
+    .findOne({_id: req.user.id}, (err, user) => {
+      if(err) {
+        req.flash('error', err.message);
+      }
+
+      res.render('dashboard/share/index', {
+        title: 'Share Gifts',
+        breadcrumbsName: 'Share'
+      });
+      console.log(user)
+    });
+});
+
 module.exports = router;
