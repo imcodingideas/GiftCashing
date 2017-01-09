b67c3ccc92186e23aad21af9701853b38bffd49e
Joseph Chambers <joseph@michael-chambers.com>
2017-01-08 23:06:17 -0700

    fix

--- mailer.js
+++ mailer.js
@@ -2,13 +2,13 @@
 const nodemailer = require('nodemailer'),
   mg = require('nodemailer-mailgun-transport');
 
-let auth = {
+const auth = {
   auth: {
     api_key: 'key-8a976a9cba0ee1e86ec07d7398b544e8',
     domain: 'mg.giftcashing.com'
   }
 }
 
-let smtpTransport = nodemailer.createTransport(mg(auth));
+const smtpTransport = nodemailer.createTransport(mg(auth));
 
 module.exports = smtpTransport;
