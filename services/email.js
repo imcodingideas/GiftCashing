ef8ffa9428028f0447f8e75b200830c1e05d08f1
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-01-29 15:12:06 -0700

    Refactor email service

--- services/email.js
+++ services/email.js
@@ -6,29 +6,27 @@ let emailOptions = {
   html: ''
 };
 
-module.exports = {
-	
-	sendForgotPassword: function (user, password){
-		emailOptions.to = user.username;
-		emailOptions.subject = 'Forgot account';
-		emailOptions.html = `
+module.exports.sendForgotPassword = function(user, password) {
+  emailOptions.to = user.username;
+  emailOptions.subject = 'Forgot account';
+  emailOptions.html = `
 	        <p>Dear ${user.firstName} ${user.lastName},</p>
 	        <p>Your password are is restored, please visit the Login Page for access with the new password.</p>
 	        <p>New password: <strong>${password}</strong></p>
 	        <p>Best Regards,<br/>
 	        GiftCashing.com</p>
     	`;
-
-		mailer.sendMail(emailOptions, (err, info) => {
-            if (err) console.log('Mailing Error: ', err);
-            console.log('mailing......................', info);
-        });
-	}
   
-  giftStatusIsReview: function (user){
-    emailOptions.to = user.username;
-    emailOptions.subject = 'You have received a new gift';
-    emailOptions.html = `
+  mailer.sendMail(emailOptions, (err, info) => {
+    if(err) console.log('Mailing Error: ', err);
+    console.log('mailing......................', info);
+  });
+};
+
+module.exports.giftStatusIsReview = function(user) {
+  emailOptions.to = user.username;
+  emailOptions.subject = 'You have received a new gift';
+  emailOptions.html = `
 	        <p>Dear ${user.firstName} ${user.lastName},</p>
 	        <p>You have received a gift from senderFirstName senderLastName.</p>
 	        <p>Access your online account and review the gift details. Then, decide whether you want to accept it.</p>
@@ -37,34 +35,34 @@ module.exports = {
 	        <p>Best Regards,<br/>
 	        GiftCashing.com</p>
     	`;
-    
-    mailer.sendMail(emailOptions, (err, info) => {
-      if (err) console.log('Mailing Error: ', err);
-      console.log('mailing......................', info);
-    });
-  }
   
-  giftStatusIsPaid: function (user){
-    emailOptions.to = user.username;
-    emailOptions.subject = 'Your have been paid.';
-    emailOptions.html = `
+  mailer.sendMail(emailOptions, (err, info) => {
+    if(err) console.log('Mailing Error: ', err);
+    console.log('mailing......................', info);
+  });
+};
+
+module.exports.giftStatusIsPaid = function(user) {
+  emailOptions.to = user.username;
+  emailOptions.subject = 'Your have been paid.';
+  emailOptions.html = `
 	        <p>Dear ${user.firstName} ${user.lastName},</p>
 	        <p>Great News! You have been paid for your gift. The money is on the way based on your payment preference.</p>
 	        <p>Contact us with any questions/concerns at support@giftcashing.com.</p>
 	        <p>Best Regards,<br/>
 	        GiftCashing.com</p>
     	`;
-    
-    mailer.sendMail(emailOptions, (err, info) => {
-      if (err) console.log('Mailing Error: ', err);
-      console.log('mailing......................', info);
-    });
-  }
   
-  giftStatusIsReviewOverSevenDays: function (user){
-    emailOptions.to = user.username;
-    emailOptions.subject = 'Hurry and Claim Your Gift!';
-    emailOptions.html = `
+  mailer.sendMail(emailOptions, (err, info) => {
+    if(err) console.log('Mailing Error: ', err);
+    console.log('mailing......................', info);
+  });
+};
+
+module.exports.giftStatusIsReviewOverSevenDays = function(user) {
+  emailOptions.to = user.username;
+  emailOptions.subject = 'Hurry and Claim Your Gift!';
+  emailOptions.html = `
 	        <p>Dear ${user.firstName} ${user.lastName},</p>
 	        <p>Don’t forget, you have received a gift from senderFirstName senderLastName</p>
 	        <p>Access your online account and review the gift details. Then, decide whether you want to accept it.</p>
@@ -73,11 +71,9 @@ module.exports = {
 	        <p>Best Regards,<br/>
 	        GiftCashing.com</p>
     	`;
-    
-    mailer.sendMail(emailOptions, (err, info) => {
-      if (err) console.log('Mailing Error: ', err);
-      console.log('mailing......................', info);
-    });
-  }
-	
+  
+  mailer.sendMail(emailOptions, (err, info) => {
+    if(err) console.log('Mailing Error: ', err);
+    console.log('mailing......................', info);
+  });
 };
