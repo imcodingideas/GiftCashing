46b49ee3bd59319788b007b8e9e7bf0ccc7bc000
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-01-29 21:51:24 -0700

    Added models and query to see users gifts

--- services/cronjobs.js
+++ services/cronjobs.js
@@ -1,90 +1,100 @@
-const   CronJob = require('cron').CronJob,
-        mailService = require('./email');
-
+const
+  CronJob = require('cron').CronJob,
+  User = require('../models/user'),
+  Gift = require('../models/gift'),
+  locus = require('locus'),
+  mailService = require('./email');
 
 
 module.exports.runJobs = function() {
-
+  
+  /**
+   * giftStatusIsReview
+   */
+  new CronJob({
     /**
-     * giftStatusIsReview
+     * Runs every day
+     * at 08:00:00 AM.
      */
-    new CronJob({
-        /**
-         * Runs every day
-         * at 08:00:00 AM.
-         */
-        cronTime: '00 00 08 * * *',
-        onTick: function() {
-
-            // TODO: Repleace this with query data set fo giftStatusIsReview
-            let users = [{
-                firstName: 'Isaac',
-                lastName: 'Peraza',
-                username : 'isaac.peraza@gmail.com',
-                password: '1234567890'
-            }];
-
-            users.forEach(function(user) {
-                mailService.giftStatusIsReview(user);
-            })
-        },
-        start: true,
-        timeZone: 'UTC'
-    });
-    
-
+    cronTime: '00 00 08 * * *',
+    onTick: function() {
+      
+      // TODO: Repleace this with query data set fo giftStatusIsReview
+      let users = [{
+        firstName: 'Isaac',
+        lastName: 'Peraza',
+        username: 'isaac.peraza@gmail.com',
+        password: '1234567890'
+      }];
+  
+      Gift
+        .find(query)
+        .populate('user')
+        .exec((err, gifts) => {
+          gifts.forEach(function(gift) {
+            eval(locus);
+            
+            mailService.giftStatusIsReview(gift);
+          });
+        });
+    },
+    start: true,
+    timeZone: 'UTC'
+  });
+  
+  
+  /**
+   * giftStatusIsPaid
+   */
+  new CronJob({
     /**
-     * giftStatusIsPaid
+     * Runs every day
+     * at 08:00:00 AM.
      */
-    new CronJob({
-        /**
-         * Runs every day
-         * at 08:00:00 AM.
-         */
-        cronTime: '00 00 08 * * *',
-        onTick: function() {
-
-            // TODO: Repleace this with query data set fo giftStatusIsPaid
-            let users = [{
-                firstName: 'Isaac',
-                lastName: 'Peraza',
-                username : 'isaac.peraza@gmail.com',
-                password: '1234567890'
-            }];
-
-            users.forEach(function(user) {
-                mailService.giftStatusIsPaid(user);
-            })
-        },
-        start: true,
-        timeZone: 'UTC'
-    });
-
-
+    cronTime: '00 00 08 * * *',
+    onTick: function() {
+      
+      // TODO: Repleace this with query data set fo giftStatusIsPaid
+      let users = [{
+        firstName: 'Isaac',
+        lastName: 'Peraza',
+        username: 'isaac.peraza@gmail.com',
+        password: '1234567890'
+      }];
+      
+      users.forEach(function(user) {
+        mailService.giftStatusIsPaid(user);
+      })
+    },
+    start: true,
+    timeZone: 'UTC'
+  });
+  
+  
+  /**
+   * giftStatusIsReviewOverSevenDays
+   */
+  new CronJob({
     /**
-     * giftStatusIsReviewOverSevenDays
+     * Runs every day
+     * at 08:00:00 AM.
      */
-    new CronJob({
-        /**
-         * Runs every day
-         * at 08:00:00 AM.
-         */
-        cronTime: '00 00 08 * * *',
-        onTick: function() {
-
-            // TODO: Repleace this with query data set fo giftStatusIsReviewOverSevenDays
-            let users = [{
-                firstName: 'Isaac',
-                lastName: 'Peraza',
-                username : 'isaac.peraza@gmail.com',
-                password: '1234567890'
-            }];
-
-            users.forEach(function(user) {
-                mailService.giftStatusIsReviewOverSevenDays(user);
-            })
-        },
-        start: true,
-        timeZone: 'UTC'
-    });
+    cronTime: '00 00 08 * * *',
+    onTick: function() {
+      
+      // TODO: Repleace this with query data set fo giftStatusIsReviewOverSevenDays
+      let users = [{
+        firstName: 'Isaac',
+        lastName: 'Peraza',
+        username: 'isaac.peraza@gmail.com',
+        password: '1234567890'
+      }];
+      
+      users.forEach(function(user) {
+        mailService.giftStatusIsReviewOverSevenDays(user);
+      })
+    },
+    start: true,
+    timeZone: 'UTC'
+  });
 };
