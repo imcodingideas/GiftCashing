219edc7ea27bb3ea9a13088fb8d837427b6379af
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-02-04 17:35:50 -0700

    Refactor Index Route to use mailService

--- routes/index.js
+++ routes/index.js
@@ -54,37 +54,7 @@ router.post(
           res.redirect('back');
         }
         
-        // Send the Welcome Email
-        let welcomeEmail;
-        welcomeEmail = `
-	        <p><strong>Thank You for Signing Up!</strong></p>
-	        <p>Very soon you'll be able to receive gifts from our site.</p>
-	        <p>We are going to add your Alias to our Members listing on SendGiftHere.com and then, send you a confirmation E-mail.</p>
-	        <p>Once added, tell your Friends & Admirers to use SendGiftHere.com to send you a gift.  Also, don’t forget to list SendGiftHere.com as your preferred wish list site.</p>
-	        <p>When gifts are purchased to your Alias, an email will be sent and you will be given the option of either cash or a purchase.</p>
-	        <p><strong>If you prefer cash,</strong> you'll receive up to 80% of the gift amount by check or PayPal. (We're adding direct deposit soon)</p>
-	        <p><strong>If you would like a purchase,</strong> just send us the gift's URL, again, valued up to 80% of the gift amount, and we'll buy it and forward, using your profile address.</p>
-	        <p>Again, we will send a separate email once you've been added and thank you for signing up!</p>
-	        <p><strong>If you don't hear from us or don't see your name in the Member list when checking out on SendGiftHere.com within two business days, contact us immediately using the contact form on our site.</strong></p>
-	        <p>Thanks Again for Signing Up!</p>
-	        <p>Best Regards,<br/>
-	        GiftCashing.com</p>
-	        <p>Support Team</p>
-	        <p>GiftCashing.com</p>
-    	`;
-        
-        let emailOptions = {
-          from: 'joseph@michael-chambers.com',
-          to: newUser.username,
-          subject: 'Thank You for Signing Up!',
-          html: welcomeEmail
-        };
-        
-        mailer
-          .sendMail(emailOptions, (err, info) => {
-            if(err) console.log('Mailing Error: ', err);
-            console.log('mailing......................', info);
-          });
+        emailService.registration(user);
         
         passport
           .authenticate('local')(req, res, () => {
@@ -125,9 +95,6 @@ router.post(
           if(err) {
             console.log('lastLoginDate error update: ', err);
           }
-          /**
-           * Are we saving this?
-           */
           console.log('lastLoginDate success update');
         });
     
--- services/email.js
+++ services/email.js
@@ -6,7 +6,7 @@ let emailOptions = {
   html: ''
 };
 
-var sendMail = function (emailOptions) {
+let sendMail = function (emailOptions) {
   mailer.sendMail(emailOptions, (err, info) => {
     if (err) console.log('Mailing Error: ', err);
     console.log('mailing......................', info);
@@ -77,14 +77,35 @@ module.exports.sendContact = function(contact) {
   emailOptions.to = 'joseph@michael-chambers.com';
   emailOptions.subject = 'Gift Cashing | Contact';
   emailOptions.html = `
-          <p>You have a new message of contact</p>
+          <p>You have a new message from the contact form:</p>
           <p>Email: ${contact.email}</p>
           <p>Message:</p>
-          <p>
-            ${contact.message}
-            <br/>
-            GiftCashing.com
-          </p>
-      `;
+          <p>${contact.message} <br/>
+            GiftCashing.com </p>
+        `;
+  sendMail(emailOptions);
+};
+
+
+module.exports.registration = function(user) {
+  eval(locus);
+  emailOptions.to = 'user.username';
+  emailOptions.subject = 'Thank You for Signing Up!';
+  emailOptions.html = `
+	        <p><strong>Thank You for Signing Up!</strong></p>
+	        <p>Very soon you'll be able to receive gifts from our site.</p>
+	        <p>We are going to add your Alias to our Members listing on SendGiftHere.com and then, send you a confirmation E-mail.</p>
+	        <p>Once added, tell your Friends & Admirers to use SendGiftHere.com to send you a gift.  Also, don’t forget to list SendGiftHere.com as your preferred wish list site.</p>
+	        <p>When gifts are purchased to your Alias, an email will be sent and you will be given the option of either cash or a purchase.</p>
+	        <p><strong>If you prefer cash,</strong> you'll receive up to 80% of the gift amount by check or PayPal. (We're adding direct deposit soon)</p>
+	        <p><strong>If you would like a purchase,</strong> just send us the gift's URL, again, valued up to 80% of the gift amount, and we'll buy it and forward, using your profile address.</p>
+	        <p>Again, we will send a separate email once you've been added and thank you for signing up!</p>
+	        <p><strong>If you don't hear from us or don't see your name in the Member list when checking out on SendGiftHere.com within two business days, contact us immediately using the contact form on our site.</strong></p>
+	        <p>Thanks Again for Signing Up!</p>
+	        <p>Best Regards,<br/>
+	        GiftCashing.com</p>
+	        <p>Support Team</p>
+	        <p>GiftCashing.com</p>
+    	`;
   sendMail(emailOptions);
 };
